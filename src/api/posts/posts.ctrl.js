7236c478d38475b1ba721ae2ef451850f090ad9d
이용준 <yongjun.lee@probit.com>
2021-01-15 11:02:04 +0900

    joi 적용 후

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/Post';
 import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
 
 // ObjectId의 형식을 검사하는 미들웨어 작성
 const { ObjectId } = mongoose.Types;
@@ -22,6 +23,21 @@ POST /api/posts
 }
  */
 export const write = async ctx => {
+  //---------------------------------------------------------------------------
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있는지 검증
+    title: Joi.string().required(), // required()가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(), // 문자로 이루어진 배열
+  });
+  // 검증하고 나서 실패인 경우 에러처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+  //--------------------------------------------------------------------------
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -85,6 +101,20 @@ PATCH /api/posts/:id
 */
 export const update = async ctx => {
   const { id } = ctx.params;
+  // --------------------------------------------------------------------
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+  // 검증하고 나서 에러처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+  // --------------------------------------------------------------------
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
